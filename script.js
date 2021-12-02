68f05d1ca0f2e6729d12e5d3be1932b5051a6f92
Smulimaan <samuli.tyyska@live.com>
2021-12-03 00:16:53 +0200

    commit 3.12.1

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-//fetch('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=1616450400&to=1616536800' )
- // .then((response) => {
-   // return response.text();
-  //})
-  //.then((data) => {
-    // do something with 'data'
- //   document.querySelector('.bitcoinApi').innerHTML = data;
-    //console.log(data);
- // });
  
   function bitcoin1() {
       
-      //console.log(document.getElementById("start").value);
-      //console.log(Date.parse(document.getElementById("start").value)/1000);
-      //console.log(document.getElementById("end").value);
-      //console.log(Date.parse(document.getElementById("end").value)+86400000)/1000;
       let firstdate = Date.parse(document.getElementById("start").value)/1000;
       let lastdate = (Date.parse(document.getElementById("end").value)+86400000)/1000;
       let i=firstdate;
@@ -22,17 +9,15 @@
       console.log("firsdate: "+firstdate);
       console.log("lastdate: "+lastdate);
       console.log("i: "+i);
-        //while (i < lastdate) {
+        
             
             let yesterdaysPrice = 0;
             let bearish = 0;
             let trendStart = 0;
-            let trendEnd = 0;
             let daysPrice = 0;
             let maxBearish = 0;
             let maxStart = 0;
-            //let currentMax = {bearish: 0, trendStart: 0, trendEnd: lastdate}
-            //const currentMax=[];
+            
         
         
       fetch(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=${firstdate}}&to=${(lastdate+86400)}`)
@@ -41,9 +26,6 @@
       return response.json();
       })
       .then((data) => {
-    // do something with 'data'
-        //console.log(document.querySelector('.bitcoinApi').innerHTML = data);
-        //console.log(data);
 
         const obj = data;
         console.log(obj.prices);
@@ -67,21 +49,9 @@
             if (found != undefined) {
                 daysPrice=(obj.prices[i])[1];
                 console.log("Daysprice: "+daysPrice)
-                break
+                break;
             }
             
-            //const index = obj.prices.findIndex(element => element > thisDate);
-            //console.log(index);
-
-            
-            
-            // if (thisDate-found < 0) {
-            //     daysTime=found;
-            //     console.log("closest päivitetty: "+daysTime);
-                
-            // }else {
-            // continue;
-            // }
             
         }
         if (daysPrice < yesterdaysPrice && bearish==0) {
@@ -105,24 +75,7 @@
             
             
         } 
-        // else if(bearish===maxBearish && daysPrice > yesterdaysPrice) {
-        //         bearish = 0;
-        //         trendEnd = thisDate;
-        //         }
         
-        
-
-            
-            
-
-            
-
-           
-           
-           
-           
-            
-            //console.log(firstdate)
             yesterdaysPrice = daysPrice;
             
             
@@ -155,5 +108,59 @@ document.getElementById("tehtava1").innerHTML = "The longest bearish Bitcoin tre
 
   
 
+  }
+
+  function bitcoin2 (){
+    let firstdate = Date.parse(document.getElementById("start2").value)/1000;
+    let lastdate = (Date.parse(document.getElementById("end2").value)+86400000)/1000;
+    let timeWindow = (lastdate - firstdate)/86400;
+    let maxVolume = 0;
+
+    fetch(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=${firstdate}}&to=${(lastdate+86400)}`)
+      .then((response) => {
+        console.log(response.status);
+      return response.json();
+      })
+      .then((data) => {
+
+        const obj = data;
+        console.log(obj);
+        console.log(obj.total_volumes);
+
+        for (let i=0; i<timeWindow; i++) {
+            
+            thisDate=firstdate*1000+(i*86400000);
+            
+            
+            for(let i=0; i < obj.total_volumes.length; i++) {
+
+                const found = obj.total_volumes[i].find(element => element > thisDate);
+                console.log("found: "+found);
+                if (found != undefined) {
+                    console.log((obj.total_volumes[i])[1]);
+                    if((obj.total_volumes[i])[1]>maxVolume) {
+                        maxVolume=(obj.total_volumes[i])[1];
+                    console.log("maxVolume: "+maxVolume)
+                    
+                    }
+                    break;
+                }
+                
+                
+            }
+
+            // const found = obj.total_volumes[i].find(element => element > thisDate);
+            // console.log("found: "+found);
+            // if (found != undefined) {
+            //     daysPrice=(obj.prices[i])[1];
+            //     console.log("Daysprice: "+daysPrice)
+            //     break
+            // }
+            
+            
+        }
+           console.log("maksimivola: "+maxVolume)
+        
+    });
   }
 
